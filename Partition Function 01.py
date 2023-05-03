9de7d11b909fe258d47d4194542a7ca8fd56f7a9
lifeisstrange1 <132474281+lifeisstrange1@users.noreply.github.com>
2023-05-03 22:11:00 +0530

    Update Partition Function 01.py

--- Partition Function 01.py
+++ Partition Function 01.py
@@ -40,11 +40,9 @@ T1 = np.linspace(0.0001,5000,1000)
 T2 = np.linspace(5000,10**6,1000)
 
 # 2 level
-E = [0,1,2]
-g=[1,1,1]
+E = [0,1,]
+g=[1,1,]
 
-E_mid = np.sum(E)/len(E)
-E_mid = np.full(shape = len(T2) ,fill_value = E_mid)
 
 plt.plot(T1,Z(g,E,T1))
 plt.xlabel("T")
@@ -64,7 +62,7 @@ plt.show()
 
 plt.plot(T1,frac(g,E,T1)[0], label = "for energy = "+str(E[0]) + " eV")
 plt.plot(T1,frac(g,E,T1)[1], label = "for energy = "+str(E[1]) + " eV")
-plt.plot(T1,frac(g,E,T1)[2], label = "for energy = "+str(E[2]) + " eV")
+#plt.plot(T1,frac(g,E,T1)[2], label = "for energy = "+str(E[2]) + " eV")
 plt.xlabel("T")
 plt.ylabel("N_j / N")
 plt.legend(loc="best")
@@ -72,9 +70,12 @@ plt.grid()
 plt.title("N_j/N vs T (low T)")
 plt.show()
 
+
 plt.plot(T2,frac(g,E,T2)[0], label = "for energy = "+str(E[0]) + " eV")
 plt.plot(T2,frac(g,E,T2)[1], label = "for energy = "+str(E[1]) + " eV")
-plt.plot(T2,frac(g,E,T2)[2], label = "for energy = "+str(E[2]) + " eV")
+#plt.plot(T2,frac(g,E,T2)[2], label = "for energy = "+str(E[2]) + " eV")
+E_mid = np.sum(E)/len(E)
+E_mid = np.full(shape = len(T2) ,fill_value = E_mid)
 plt.plot(T2,E_mid,'--')
 plt.xlabel("T")
 plt.ylabel("N_j / N")
@@ -154,6 +155,163 @@ plt.title("F vs T (high T)")
 plt.show()
 
 
+
+
+
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+k = 8.617 * 10**(-5) # eV/K
+
+def Z(g,E,T):
+    n = len(E)
+    part = []
+    
+    for j in (T):
+        
+        z = 0
+
+        for i in range(n):
+
+            z = z + g[i]*np.exp(-E[i]/(k*j))
+
+        part.append(z)
+
+    return part
+
+def frac(g,E,T):
+    n = len(E)
+    FRAC = np.zeros(n*len(T)).reshape(n, len(T))
+    z = Z(g,E,T)
+    
+    for i in range(len(T)):
+
+        for j in range(n):
+            
+            f1 = (g[j]*np.exp(-E[j]/(k*T[i]))) / z[i]
+            FRAC[j][i] = f1
+            
+        
+        
+    return FRAC
+
+
+T1 = np.linspace(0.0001,5000,1000)
+T2 = np.linspace(5000,10**6,1000)
+
+# 3 level
+E = [0,1,2]
+g=[1,1,1]
+
+
+plt.plot(T1,Z(g,E,T1))
+plt.xlabel("T")
+plt.ylabel("Z")
+plt.title("Z vs T (low T)")
+plt.grid()
+plt.show()
+
+plt.plot(T2,Z(g,E,T2))
+plt.xlabel("T")
+plt.ylabel("Z")
+plt.grid()
+plt.title("Z vs T (high T)")
+plt.show()
+
+
+
+plt.plot(T1,frac(g,E,T1)[0], label = "for energy = "+str(E[0]) + " eV")
+plt.plot(T1,frac(g,E,T1)[1], label = "for energy = "+str(E[1]) + " eV")
+plt.plot(T1,frac(g,E,T1)[2], label = "for energy = "+str(E[2]) + " eV")
+plt.xlabel("T")
+plt.ylabel("N_j / N")
+plt.legend(loc="best")
+plt.grid()
+plt.title("N_j/N vs T (low T)")
+plt.show()
+
+plt.plot(T2,frac(g,E,T2)[0], label = "for energy = "+str(E[0]) + " eV")
+plt.plot(T2,frac(g,E,T2)[1], label = "for energy = "+str(E[1]) + " eV")
+plt.plot(T2,frac(g,E,T2)[2], label = "for energy = "+str(E[2]) + " eV")
+plt.xlabel("T")
+plt.ylabel("N_j / N")
+plt.legend()
+plt.grid()
+plt.title("N_j/N vs T (high T)")
+plt.show()
+
+
+#INTERNAL ENERGY
+
+population_1 = frac(g,E,T1)
+population_2 = frac(g,E,T2)
+U_1,U_2 = 0,0
+for i in range(len(population_1)) :
+    U_1 += population_1[i]*E[i]
+
+for i in range(len(population_2)) :
+    U_2 += population_2[i]*E[i]
+
+
+plt.plot(T1,U_1)
+plt.xlabel("T")
+plt.ylabel("U")
+plt.title("U vs T (low T)")
+plt.grid()
+plt.show()
+
+plt.plot(T2,U_2)
+plt.xlabel("T")
+plt.ylabel("U")
+plt.grid()
+plt.title("U vs T (high T)")
+plt.show()
+
+
+
+#ENTROPY
+z1 = Z(g,E,T1)
+z2 = Z(g,E,T2)
+N = 1
+
+S1 = N*k*np.log(np.array(z1) / N) + U_1 / T1 + N*k
+S2 = N*k*np.log(np.array(z2) / N) + U_2 / T2 + N*k
+
+plt.plot(T1,S1)
+plt.xlabel("T")
+plt.ylabel("S")
+plt.title("S vs T (low T)")
+plt.grid()
+plt.show()
+
+plt.plot(T2,S2)
+plt.xlabel("T")
+plt.ylabel("S")
+plt.grid()
+plt.title("S vs T (high T)")
+plt.show()
+
+
+#HELMHOLTZ
+F1 = -N*k*np.array(T1) * np.log(np.array(z1))
+F2 = -N*k*np.array(T2) * np.log(np.array(z2))
+
+plt.plot(T1,F1)
+plt.xlabel("T")
+plt.ylabel("F")
+plt.title("F vs T (low T)")
+plt.grid()
+plt.show()
+
+plt.plot(T2,F2)
+plt.xlabel("T")
+plt.ylabel("F")
+plt.grid()
+plt.title("F vs T (high T)")
+plt.show()
 
 
 
